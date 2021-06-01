deeda83d122cbb6cca30886fb7e7dc471efd3c5c
preudh <preudhomme.patrice@gmail.com>
2021-06-01 14:29:56 +0200

    0106b

---
+++ app_users/forms.py
@@ -0,0 +1,34 @@
+"""Declaring a form is very similar to declaring a template. It is also
+from a class inheriting from a parent class provided by Django. The attributes also match
+to the form fields."""
+
+from django import forms
+
+
+# create login form
+class LoginForm(forms.Form):
+    username = forms.CharField(label="username", max_length=50)
+    password = forms.CharField(label="password", widget=forms.PasswordInput)
+
+
+# create account
+class RegisterForm(forms.Form):
+    name = forms.CharField(
+        label="Name",
+        # used the attribute form control to take advantage of bootstrap sizing components
+        widget=forms.TextInput(attrs={'class': 'form-control'}), )
+    email = forms.EmailField(
+        label='Email',
+        max_length=100,
+        widget=forms.EmailInput(attrs={'class': 'form-control'}),
+        # the field is required
+        required=True)
+    # Passwordfield does not exist so that one must use it in widget
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        required=True)
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(max_length=50)
