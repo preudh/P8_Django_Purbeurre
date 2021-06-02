c02b638014a03b218d8a5cf4ae2f2d93d307606a
preudh <preudhomme.patrice@gmail.com>
2021-06-02 11:34:29 +0200

    0206

--- P8_Django_Purbeurre/urls.py
+++ P8_Django_Purbeurre/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include # Functions for use in URLsconfs
+from django.urls import path, include  # Functions for use in URLsconfs
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     # '' means home page, when someone goes to home page the controller will go to app_manage app here we have urls.py
     path('', include('app_manage.urls')),
+    path('', include('app_users.urls')),
+    path('admin/', admin.site.urls),
 ]
---
+++ app_users/urls.py
@@ -0,0 +1,7 @@
+"""P8_Django_Purbeurre URL Configuration"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.login, name='login'),
+]
