4eaa84309f18bbcdabf15108c718afb2771deaec
preudh <preudhomme.patrice@gmail.com>
2021-06-05 19:44:34 +0200

    0506c

--- app_manage/urls.py
+++ app_manage/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # only once empty
+    # path('', views.home, name='home'),
+    # only once empty
+    path('', views.index, name='index'),
+    path('termes/', views.termes, name='termes'),
+    path('base/', views.base, name='base'),
 ]
--- app_manage/views.py
+++ app_manage/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from .forms import SearchForm
 
 
 # Create your views here.
 
-def home(request):
-    return render(request, '../templates/home.html')
+def base(request):
+    return render(request, '../templates/base.html')
+
+
+def termes(request):
+    return render(request, 'termes.html')
+
+
+def index(request):
+    form = SearchForm(request.POST)
+    context = {
+        'form': form
+    }
+    return render(request, 'index.html', context)
