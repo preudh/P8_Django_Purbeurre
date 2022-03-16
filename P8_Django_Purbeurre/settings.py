41e93597291b3c207fee47b21dfd407fe769200a
preudh <preudhomme.patrice@gmail.com>
2022-03-16 20:36:51 +0100

    1603

--- P8_Django_Purbeurre/settings.py
+++ P8_Django_Purbeurre/settings.py
@@ -131,9 +131,9 @@ USE_TZ=True  # True to take in charge local time = TIME_ZONE
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL='/static/'
+STATIC_URL='static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
